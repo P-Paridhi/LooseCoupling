49c63c94af14bbf7cfdfdc2f175ab57378a7c194
Paridhi Bhagwat <paridhibhagwat2002@gmail.com>
2026-07-10 23:26:41 +0530

    Initial commit

---
+++ SpringMvcArchitecture/src/main/java/com/paridhi/mvc/SpringMvcArchitecture/SpringMvcArchitectureApplication.java
@@ -0,0 +1,13 @@
+package com.paridhi.mvc.SpringMvcArchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcArchitectureApplication.class, args);
+	}
+
+}
